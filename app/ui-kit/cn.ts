9f719e8df32fc8aa218e6fd3d47e80eb5aa47189
Pg Gonni <59372630+sekeidesign@users.noreply.github.com>
2026-09-18 07:27:11 -0400

    Migrate cn (#50)

--- app/ui-kit/cn.ts
+++ app/ui-kit/cn.ts
@@ -1,8 +1,1 @@
-import clsx from "clsx";
-import { twMerge } from "tailwind-merge";
-
-const cn = (...inputs: Parameters<typeof clsx>) => {
-	return twMerge(clsx(inputs));
-};
-
-export { cn };
+export { cn } from "cn";
